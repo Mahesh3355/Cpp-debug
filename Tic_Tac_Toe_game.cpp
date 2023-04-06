997abd47faf97d6deb4d200744a137bc7cfc8354
mahesh balaji birajdar <maheshbirajdar3355@gmail.com>
2023-04-06 23:46:15 +0530

    corrected vertical wins

--- Tic_Tac_Toe_game.cpp
+++ Tic_Tac_Toe_game.cpp
@@ -10,7 +10,7 @@ void drawBoard() {
    cout << "*******************************   Tic Tac Toe  ******************************" << endl;
    for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
-         cout << board[i][j] << " ";
+         cout << board[i][j] << "";
       }
       cout << endl;
    }
