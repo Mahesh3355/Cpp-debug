acefc1169f57a7bbe8dab0df6749455eb8b0766f
mahesh balaji birajdar <maheshbirajdar3355@gmail.com>
2023-04-07 00:08:05 +0530

    output issue3 resolved

--- debug2.cpp
+++ debug2.cpp
@@ -1,29 +1,53 @@
 #include <iostream>
-
 using namespace std;
 
-bool is_prime(int num) {
-    if (num <= 1) {
-        return True;
+bool is_prime(int num)
+{
+  if (num <= 1 && num > 0)
+  {
+    return true;
+  }
+  // if the number is positive.
+  if (num > 0)
+  {
+    for (int i = 2; i <= num / 2; i++)
+    {
+      if (num % i == 0)
+      {
+        return true;
+      }
     }
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
-            return true;
-        }
+    return false;
+  }
+
+  // included because of negative number consen,
+  // if the numbers are only positive the no need of it.
+  else
+  {
+    for (int i = 2; i < -num / 2; i++)
+    {
+      if (num % i == 0)
+      {
+        return true;
+      }
     }
-    return False;
+    return false;
+  }
 }
 
-int main() {
-    int input;
-    cout << "Enter a number: ";
-    cin >> input;
-    if (is_prime(input)) {
-        cout << input << " is a prime number." << endl;
-    } else {
-        cout << input << " is a prime number." << endl;
-    }
-    cout << input << " is not a prime number." << endl;
-    
-    return 0;
+int main()
+{
+  int input;
+  cout << "Enter a number: ";
+  cin >> input;
+  //function call is_prime
+  if (is_prime(input)) //excutes when it returns 1 otherwise else statement gets excutes.
+  {
+    cout << input << " is a prime not number." << endl;
+  }
+  else
+  {
+    cout << input << " is a prime number." << endl;
+  }
+  return 0;
 }
